985f2c227e5db1cf7e0214a0de5c4d7836711ab1
Nikitha <nikitha.kashimata@gmail.com>
2020-08-12 20:30:20 +0530

    latest

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import pyrebase
-from flask import Flask, render_template, request, redirect, url_for
+import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 firebaseConfig = {
     "apiKey": "AIzaSyCf-90YLu78oyfdqqjBQmW5N3z41wS9R38",
@@ -13,11 +18,17 @@ firebaseConfig = {
  # Initialize Firebase
 firebase=pyrebase.initialize_app(firebaseConfig);
 auth=firebase.auth()
-db = firebase.firestore.client()
+
+# Use a service account
+cred = credentials.Certificate('C:/Users/Dell/Downloads/caterer-cd2e1-ff5caaa728f1.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 
 @app.route("/")
 def main():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route("/menu")
 def menu():
@@ -34,40 +45,55 @@ def confirm():
 
 @app.route("/reservation",methods=['POST','GET'])
 def reservation():
-    data = {
-    u'stringExample': u'Hello, World!',
-    u'booleanExample': True,
-    u'numberExample': 3.14159265,
-    # u'dateExample': datetime.datetime.now(),
-    u'arrayExample': [5, True, u'hello'],
-    u'nullExample': None,
-    u'objectExample': {
-        u'a': 5,
-        u'b': True
-    }
-    }
+                error=None
+                if request.method=='POST':
 
-    db.collection(u'data').document(u'one').set(data)
-        # error=None
+                        date=request.form['date']
+                        time=request.form['time']
+                        persons=request.form['persons']
+                        name=request.form['name']
+                        email=request.form['email']
+                        number=request.form['number']
+                        data={
+                        u'date':date,
+                        u'time':time,
+                        u'persons':persons,
+                        u'name':name,
+                        u'email':email,
+                        u'phone':number
+                        }
+                        db.collection(u'reserve').document(number).set(data)
+                        docs = db.collection(u'reserve').where(u'phone',u'==',number).stream()
+                        return render_template("retrieve.html",docs=docs)
+                        # return redirect(url_for('retrieve'))
+
+                return render_template('reservation.html',error=error)
+            
         # if request.method=='POST':
         #     #if request.form['submit']:
+        #         date=request.form['date']
+        #         time=request.form['time']
+        #         persons=request.form['persons']
+        #         name=request.form['name']
+        #         email=request.form['email']
+        #         number=request.form['number']
         #         data={
-        #         u"date":u"request.form['date']",
-        #         u"time":u"request.form['time']",
-        #         u"persons":u"request.form['persons']",
-        #         u"name":u"request.form['name']",
-        #         u"email":u"request.form['email']",
-        #         u"phone":u"request.form['number']"
+        #         u'date':date,
+        #         u'time':time,
+        #         u'persons':persons,
+        #         u'name':name,
+        #         u'email':email,
+        #         u'phone':number
         #         }
-        #         db.collection(u'reserve').document(u'1').set(data)
+        #         db.collection(u'reserve').document(u'20').set(data)
 
         #         return redirect(url_for('confirm'))
 
-        #     # except:
-        #     #     error='Please Enter all data'
-        #     #     return render_template('reservation.html',error=error)
+            # except:
+            #     error='Please Enter all data'
+            #     return render_template('reservation.html',error=error)
 
-    return render_template('reservation.html',error=error)
+        
 
 @app.route("/gallery")
 def gallery():
@@ -95,13 +121,20 @@ def forgot():
 
 @app.route("/login",methods=['POST','GET'])
 def login():
+    if 'username' in session:
+        return redirect(url_for('reservation'))
+
+    session.pop('username',None)
     error=None
     if request.method=='POST':
-        email=request.form['username']
+        username=request.form['username']
         password=request.form['password']
         
         try:
-            user=auth.sign_in_with_email_and_password(email,password)
+            user=auth.sign_in_with_email_and_password(username,password)
+            session['username']=user
+            session['logged_in'] = True
+
             return redirect(url_for('reservation'))
 
         except:
@@ -128,8 +161,17 @@ def signup():
 
     return render_template('signup.html',error=error)
 
+@app.route("/logout")
+def logout():
+    session.pop('username',None)
+    return render_template("index.html")
 
+@app.route("/retrieve")
+def retrieve():
+    # docs = db.collection(u'reserve').where(u'name',u'==',u'Raksha').stream()
+    return render_template("retrieve.html")
 
 
 if __name__ == "__main__":
+   app.secret_key='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
    app.run(debug=True)
